6ace7fd3355dfd16b2f4bb770bb63f8d31c673f7
nabildroid <pni20156789@gmail.com>
2022-11-19 20:39:19 +0100

    logo

--- app/(dashboard)/components/navigation/nav.tsx
+++ app/(dashboard)/components/navigation/nav.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { FunctionComponent } from "react";
 import NavButton from "./navbutton";
@@ -7,7 +8,9 @@ interface NavProps {}
 const Nav: FunctionComponent<NavProps> = () => {
   return (
     <div className="sticky h-screen top-0 border-r bg-slate-700  border-neutral-600 p-4 flex flex-col items-center">
-      <div className="text-4xl font-bold text-white ">A</div>
+      <Link href="https://arib.shop" className="text-4xl font-bold text-white ">
+        <Image src="/logo.png" alt="logo" width={30} height={30} />
+      </Link>
       <div className="mt-12 flex flex-col items-center space-y-4">
         <Link href="/">
           <NavButton icon="Home" />
@@ -24,7 +27,7 @@ const Nav: FunctionComponent<NavProps> = () => {
       <div className="flex-1" />
       <div>
         <Link href="/start">
-        <NavButton icon="Add" />
+          <NavButton icon="Add" />
         </Link>
       </div>
     </div>
